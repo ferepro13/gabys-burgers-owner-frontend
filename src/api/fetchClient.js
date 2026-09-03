5482f8630cdcd40c8c42a371f367b3c6fd327c78
ferepro13 <ferepro13@gmail.com>
2026-09-03 15:06:31 -0400

    fixed backend url according to environment

--- src/api/fetchClient.js
+++ src/api/fetchClient.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api"
+const BASE_URL = (import.meta.env.VITE_ENV === "production") ? (import.meta.env.VITE_API_URL || "http://localhost:3000/api") : "http://localhost:3000/api"
 
 /** 
  * Funcion q envuelve a fetch para agregar logica comun
